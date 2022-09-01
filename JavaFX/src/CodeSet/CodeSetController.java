2d2e9de0e7188225eb115e65c6dc0a1243ecce42
Ido Fassberg <idofassberg@gmail.com>
2022-09-01 21:41:57 +0300

    start work on CodeSetController

--- JavaFX/src/CodeSet/CodeSetController.java
+++ JavaFX/src/CodeSet/CodeSetController.java
@@ -1,4 +1,18 @@
 package CodeSet;
 
-public class CodeSetController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CodeSetController implements Initializable {
+
+    @FXML private VBox vb_codeSet;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
