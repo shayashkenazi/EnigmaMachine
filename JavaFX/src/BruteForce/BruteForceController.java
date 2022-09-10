dfb00c3182e1e6797a9e15699545da0c87c7092b
Ido Fassberg <idofassberg@gmail.com>
2022-09-10 14:47:51 +0300

    Tire class (Doesn't run)

--- JavaFX/src/BruteForce/BruteForceController.java
+++ JavaFX/src/BruteForce/BruteForceController.java
@@ -1,28 +1,52 @@
 package BruteForce;
 
+import DataStructures.Trie;
 import Interfaces.SubController;
 import MainApp.AppController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 
 public class BruteForceController implements SubController {
 
+    private Trie trie = new Trie();
     private AppController appController;
-    @FXML
-    private TextArea ta_test;
-    @FXML
-    private Button btn_button;
 
-    @FXML
-    void testToDeleteShayGay(ActionEvent event) {
-        appController.testToDeleteShayHomo();
+
+    @FXML private Button btn_clear, btn_proccess, btn_reset;
+
+    @FXML private TextField tf_codeConfiguration, tf_input, tf_output, tf_searchBar;
+
+
+    @FXML void clearBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML void proccessBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML void resetBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML void searchBarTextChanged(InputMethodEvent event) {
+
     }
 
     @Override
     public void setMainController(AppController mainController) {
         appController = mainController;
     }
-}
 
+    void initializeTabAfterCodeConfiguration() {
+
+        appController.initializeTrieWithDictionary();
+
+    }
+
+
+    public Trie getTrie() { return trie; }
+}
