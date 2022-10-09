4b96be30f7a05bab74c90ec53ce061096150dd37
Ido Fassberg <idofassberg@gmail.com>
2022-10-09 14:00:08 +0300

    login Controller

--- UBoat/src/login/LoginController.java
+++ UBoat/src/login/LoginController.java
@@ -1,4 +1,15 @@
 package login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LoginController {
+
+    @FXML private Button btn_login;
+
+    @FXML void loginBtnClick(ActionEvent event) {
+
+    }
+
 }
