5182f595dd2d398ab2e7a91e4f8463d208542bda
Ido Fassberg <idofassberg@gmail.com>
2022-10-09 14:06:25 +0300

    improvments

--- UBoat/src/login/LoginController.java
+++ UBoat/src/login/LoginController.java
@@ -3,13 +3,15 @@ package login;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
     @FXML private Button btn_login;
+    @FXML private TextField tf_userName;
 
     @FXML void loginBtnClick(ActionEvent event) {
-
+        uBoatMainController.func(tf_userName.getText());
     }
 
 }
